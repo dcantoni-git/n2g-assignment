dcef102f5fc3f78f9905285c60ccbea7625fb571
Dimitris Antoniadis <dc.antoniadis@gmail.com>
2023-02-13 13:32:04 +0200

    Added test for create user.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -12,11 +12,14 @@ class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
         """Test creating a user with an email is successful."""
         email = 'test@example.com'
+        username = 'test'
         password = 'testpass123'
         user = get_user_model().objects.create_user(
             email=email,
+            username=username,
             password=password,
         )
 
         self.assertEqual(user.email, email)
+        self.assertEqual(user.username, username)
         self.assertTrue(user.check_password(password))
