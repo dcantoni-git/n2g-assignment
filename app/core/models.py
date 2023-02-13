b19fd334e4098dab479b2dd5b1e264313a45c1c2
Dimitris Antoniadis <dc.antoniadis@gmail.com>
2023-02-13 13:08:28 +0200

    Added core app.

--- app/core/models.py
+++ app/core/models.py
@@ -1,1 +1,55 @@
-from django.db import models  # noqa
+from django.contrib.auth.models import UserManager
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.core.validators import MaxValueValidator, MinValueValidator
+import datetime
+
+
+class UserProfileManager(UserManager):
+    """Manager for user profiles"""
+
+    def create_user(self, email, username, password):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username)
+        user.set_password(password)
+        user.save(using='default')
+        return user
+
+    def create_superuser(self, email, username, password):
+        """Create and save a new superuser"""
+        user = self.create_user(email, username, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using='default')
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Database model for users in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    username = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def get_full_name(self):
+        """Retrieve full name of user"""
+        full_name = self.username + ' --> ' + str(self.email)
+        return full_name
+
+    def get_short_name(self):
+        """Retrieve short name of user"""
+        return self.username
+
+    def __str__(self):
+        """Return string representation of user"""
+        return str(self.email)
