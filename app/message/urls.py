bc63bcf4c894d2716c9c0700c2aa1f698e7df124
Dimitris Antoniadis <dc.antoniadis@gmail.com>
2023-02-16 04:49:47 +0200

    Modifications for send-to-exchange endpoint.

--- app/message/urls.py
+++ app/message/urls.py
@@ -9,8 +9,8 @@ from message import views
 app_name = 'message'
 
 urlpatterns = [
-    path('send-to-exchange/', views.SendToExchangeView.as_view(), name='send_to_exchange'),  # noqa: E501
-    path('store-to-database-continuous/', views.StoreToDatabaseContinuousView.as_view(), name='store_to_database_continuous'),  # noqa: E501
+    path('send-to-exchange/<int:number_of_messages>', views.SendToExchangeView.as_view(), name='send_to_exchange'),  # noqa: E501
     path('store-to-database/<int:number_of_messages>', views.StoreToDatabaseView.as_view(), name='store_to_database'),  # noqa: E501
+    path('store-to-database-continuous/', views.StoreToDatabaseContinuousView.as_view(), name='store_to_database_continuous'),  # noqa: E501
     path('show-stored-messages/', views.ShowStoredMessagesView.as_view(), name='show_stored_messages'),  # noqa: E501
 ]
--- app/message/views.py
+++ app/message/views.py
@@ -13,6 +13,7 @@ import requests
 from requests.exceptions import HTTPError
 import pika
 import json
+import time
 
 
 def connect_to_rabbitmq():
@@ -42,63 +43,73 @@ class SendToExchangeView(APIView):
     authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
 
-    def get(self, request, format=None):
+    def get(self, request, number_of_messages, format=None):
         """Consumes data from the API and send them to RabbitMQ exchange."""
 
         # Get data from the external API.
         external_api_url = 'https://xqy1konaa2.execute-api.eu-west-1.amazonaws.com/prod/results'  # noqa: E501
-        try:
-            response = requests.get(external_api_url)
-            response.raise_for_status()
-            data = response.json()
-        except HTTPError as e:
-            return Response(f'HTTPError: {e}', response.status_code)
-        except Exception as e:
-            return Response(f'Error: {e}', response.status_code)
-
-        gatewayEui_dec = int(data['gatewayEui'], 16)
-        profileId_dec = int(data['profileId'], 16)
-        endpointId_dec = int(data['endpointId'], 16)
-        clusterId_dec = int(data['clusterId'], 16)
-        attributeId_dec = int(data['attributeId'], 16)
-
-        routing_key = f"{gatewayEui_dec}.{profileId_dec}.{endpointId_dec}.{clusterId_dec}.{attributeId_dec}"  # noqa: E501
-
-        body_dict = {
-            'user_id': request.user.id,
-            'value': data['value'],
-            'timestamp': data['timestamp'],
-        }
-
-        body_json = json.dumps(body_dict, indent=4)
-
         connection, channel = connect_to_rabbitmq()
 
-        try:
-            channel.basic_publish(
-                exchange='cand_upk1',
-                routing_key=routing_key,
-                body=body_json,
-                properties=pika.BasicProperties(content_type='application/json')  # noqa: E501
-                )
-            channel.close()
-            connection.close()
-            publish_msg = 'Message was sent to exchange successfully!!'
-        except pika.exceptions.UnroutableError as e:
-            error_msg = {
-                'error_msg': f"Message publish failed! (gatewayEui: {data['gatewayEui']})",  # noqa: E501
-                'unroutable_error': str(e)
-            }
-            return Response(error_msg, status.HTTP_400_BAD_REQUEST)
-        except Exception as e:
-            error_msg = {
-                'error_msg': f"Message publish failed! (gatewayEui: {data['gatewayEui']})",  # noqa: E501
-                'details': str(e)
-            }
-            return Response(error_msg, status.HTTP_400_BAD_REQUEST)
-        data.update({'publish_msg': publish_msg})
+        for i in range(number_of_messages):
+            try:
+                response = requests.get(external_api_url)
+                response.raise_for_status()
+                data = response.json()
+            except HTTPError as e:
+                return Response(f'HTTPError: {e}', response.status_code)
+            except Exception as e:
+                return Response(f'Error: {e}', response.status_code)
 
-        return Response(data, status.HTTP_200_OK)
+            gatewayEui_dec = int(data['gatewayEui'], 16)
+            profileId_dec = int(data['profileId'], 16)
+            endpointId_dec = int(data['endpointId'], 16)
+            clusterId_dec = int(data['clusterId'], 16)
+            attributeId_dec = int(data['attributeId'], 16)
+
+            routing_key = f"{gatewayEui_dec}.{profileId_dec}.{endpointId_dec}.{clusterId_dec}.{attributeId_dec}"  # noqa: E501
+
+            body_dict = {
+                'user_id': request.user.id,
+                'value': data['value'],
+                'timestamp': data['timestamp'],
+            }
+
+            body_json = json.dumps(body_dict, indent=4)
+
+            try:
+                channel.basic_publish(
+                    exchange='cand_upk1',
+                    routing_key=routing_key,
+                    body=body_json,
+                    properties=pika.BasicProperties(content_type='application/json')  # noqa: E501
+                    )
+            except pika.exceptions.UnroutableError as e:
+                error_msg = {
+                    'error_msg': f"Message publish failed! (gatewayEui: {data['gatewayEui']})",  # noqa: E501
+                    'unroutable_error': str(e)
+                }
+                return Response(error_msg, status.HTTP_400_BAD_REQUEST)
+            except Exception as e:
+                error_msg = {
+                    'error_msg': f"Message publish failed! (gatewayEui: {data['gatewayEui']})",  # noqa: E501
+                    'details': str(e)
+                }
+                return Response(error_msg, status.HTTP_400_BAD_REQUEST)
+            time.sleep(0.1)  # Delay between API calls
+
+        channel.close()
+        connection.close()
+
+        if number_of_messages == 1:
+            publish_msg = {
+                'publish_msg': '1 message was sent to the exchange.'
+            }
+        else:
+            publish_msg = {
+                'publish_msg': f'{number_of_messages} messages were sent to the exchange.'
+            }
+
+        return Response(publish_msg, status.HTTP_200_OK)
 
 
 class StoreToDatabaseView(APIView):
@@ -111,8 +122,8 @@ class StoreToDatabaseView(APIView):
         """Get the data from the RabbitMQ queue and store them to database."""
         connection, channel = connect_to_rabbitmq()
         messages_stored_in_db = 0
-        try:
-            for i in range(number_of_messages):
+        for i in range(number_of_messages):
+            try:
                 method_frame, header, body = channel.basic_get(queue='cand_upk1_results', auto_ack=True)  # noqa: E501
                 if body is not None:
                     body_dict = json.loads(body)
@@ -133,30 +144,31 @@ class StoreToDatabaseView(APIView):
 
                     if messages_stored_in_db == number_of_messages:
                         break
+                else:
+                    break
+            except ValueError as e:
+                error_msg = {
+                    'value_error': str(e)
+                }
+                return Response(error_msg, status.HTTP_400_BAD_REQUEST)
+            except Exception as e:
+                error_msg = {
+                    'error_msg': str(e)
+                }
+                return Response(error_msg, status.HTTP_400_BAD_REQUEST)
 
-            channel.close()
-            connection.close()
-            if messages_stored_in_db == 0:
-                db_msg = "There are no messages in the queue."
-            elif messages_stored_in_db == 1:
-                db_msg = "1 message was stored in the database."
-            else:
-                db_msg = f'{messages_stored_in_db} message were stored in the database.'  # noqa: E501
-            res = {
-                'db_msg': db_msg
-            }
-            return Response(res, status.HTTP_200_OK)
-
-        except ValueError as e:
-            error_msg = {
-                'value_error': str(e)
-            }
-            return Response(error_msg, status.HTTP_400_BAD_REQUEST)
-        except Exception as e:
-            error_msg = {
-                'error_msg': str(e)
-            }
-            return Response(error_msg, status.HTTP_400_BAD_REQUEST)
+        channel.close()
+        connection.close()
+        if messages_stored_in_db == 0:
+            db_msg = "There are no messages in the queue."
+        elif messages_stored_in_db == 1:
+            db_msg = "1 message was stored in the database."
+        else:
+            db_msg = f'{messages_stored_in_db} message were stored in the database.'  # noqa: E501
+        res = {
+            'db_msg': db_msg
+        }
+        return Response(res, status.HTTP_200_OK)
 
 
 class StoreToDatabaseContinuousView(APIView):
